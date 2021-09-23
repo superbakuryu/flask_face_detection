9501074fde33e4f1e854a146685eb53504a9b646
superbakuryu <superbakuryu@gmail.com>
2021-09-23 07:45:33 +0700

    repush

--- main.py
+++ main.py
@@ -104,4 +104,4 @@ def draw_rectangle(img, rect):
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=8080)
+    app.run(host='0.0.0.0', debug=True, port=80)
